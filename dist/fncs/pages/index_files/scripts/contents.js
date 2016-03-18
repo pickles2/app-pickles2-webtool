3ee7163cbec32ad42d75e1e6827184e2d07d2795
Tomoya Koyanagi <tomk79@gmail.com>
2016-03-18 19:35:34 +0900

    pages に path と id の表示を追加

--- dist/fncs/pages/index_files/scripts/contents.js
+++ dist/fncs/pages/index_files/scripts/contents.js
@@ -5,22 +5,44 @@ $(window).load(function(){
 		{},
 		function(sitemap){
 			// console.log(sitemap);
-			var $ul = $('<ul>');
+			var $ul = $('<table>');
 			$('.contents').html('').append($ul);
+			$ul
+				.append( $('<thead>')
+					.append( $('<tr>')
+						.append( $('<th>').text('id') )
+						.append( $('<th>').text('title') )
+						.append( $('<th>').text('path') )
+					)
+				)
+			;
 			for( var path in sitemap ){
-				var $li = $('<li>');
+				var $li = $('<tr>');
 				$li
-					.append( $('<a>')
-						.text(sitemap[path].title)
-						.attr({
-							'href': 'javascript:;',
-							'data-page-path': path
-						})
-						.click(function(){
-							var $this = $(this);
-							window.open( '/mods/editor/index.html?page_path='+encodeURIComponent( $this.attr('data-page-path') ) );
-						})
-				);
+					.append( $('<th>')
+						.append( $('<span>')
+							.text(sitemap[path].id)
+						)
+					)
+					.append( $('<td>')
+						.append( $('<a>')
+							.text(sitemap[path].title)
+							.attr({
+								'href': 'javascript:;',
+								'data-page-path': path
+							})
+							.click(function(){
+								var $this = $(this);
+								window.open( '/mods/editor/index.html?page_path='+encodeURIComponent( $this.attr('data-page-path') ) );
+							})
+						)
+					)
+					.append( $('<td>')
+						.append( $('<span>')
+							.text(sitemap[path].path)
+						)
+					)
+				;
 				$ul.append($li);
 			}
 		}
