5f27b2ab98f8c2d2c507a8993b3d13919ba4d7b8
Tomoya Koyanagi <tomk79@gmail.com>
2016-09-20 04:00:30 +0900

    ログイン周辺処理整理

--- src/index_files/scripts/contents.js
+++ src/index_files/scripts/contents.js
@@ -2,71 +2,38 @@ window.contApp = new (function(){
 })();
 $(window).load(function(){
 
-	$.ajax({
-		'type': 'GET',
-		'url': '/apis/getLoginUserInfo',
-		'success': function(data, dataType){
-			// console.log(data, dataType);
-			if( !data ){
-				var html = $('#template-login-form').html();
-				$('.contents').html(html);
-				notLogin();
-			}else{
-				window.location.href = '/fncs/pages/'; // ログインしてたら pages を開く。
-
-				var html = $('#template-login').html();
-				$('.contents').html(html);
-				login();
-			}
-		},
-		'error': function(data, dataType){
-			// console.log(data, dataType);
-		},
-		'complete': function(xhr, textStatus){
-		}
-	});
-
-	/**
-	 * ログイン状態のとき
-	 */
-	function login(){
-		$('.logout-button').click(function(){
-			window.main.logout();
-		});
+	if( !$('.login-form').size() ){
+		// ログイン済みの場合
+		window.location.href = '/fncs/pages/';
 	}
 
-	/**
-	 * 非ログイン状態のとき
-	 */
-	function notLogin(){
-		$('.login-form')
-			.submit(function(){
-				var $this = $(this);
-				var data = {
-					'id': $this.find('input[type=text]').val(),
-					'pw': $this.find('input[type=password]').val()
-				};
+	$('.login-form')
+		.submit(function(){
+			var $this = $(this);
+			var data = {
+				'id': $this.find('input[type=text]').val(),
+				'pw': $this.find('input[type=password]').val()
+			};
 
-				$.ajax({
-					'type': 'POST',
-					'url': '/apis/login',
-					'data': data,
-					'success': function(data, dataType){
-						// console.log(data, dataType);
-					},
-					'complete': function(xhr, textStatus){
-						if(xhr.status == 200){
-							// alert('OK');
-							window.location.reload();
-						}else if(xhr.status == 403){
-							alert('FAILED');
-						}else{
-							alert('unknown code: '+xhr.status);
-						}
+			$.ajax({
+				'type': 'POST',
+				'url': '/apis/login',
+				'data': data,
+				'success': function(data, dataType){
+					// console.log(data, dataType);
+				},
+				'complete': function(xhr, textStatus){
+					if(xhr.status == 200){
+						// alert('OK');
+						window.location.reload();
+					}else if(xhr.status == 403){
+						alert('FAILED');
+					}else{
+						alert('unknown code: '+xhr.status);
 					}
-				});
-			})
-		;
-	}
+				}
+			});
+		})
+	;
 
 });
