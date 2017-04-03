36a50105c8871b9b9886d72bd0d4bcb792ffc535
Tomoya Koyanagi <tomk79@gmail.com>
2017-04-03 22:38:48 +0900

    サイトマップが巨大な場合の初回読み込みの負荷を低減
    - サイトマップ全体のロードを、検索時のみに絞った

--- src/fncs/pages/index_files/scripts/contents.js
+++ src/fncs/pages/index_files/scripts/contents.js
@@ -8,6 +8,7 @@ window.cont = new (function(){
 	var timerFilter;
 	var keyword = '';
 	var current_page = '';
+	_this.sitemap = false;
 
 	/**
 	 * 画面を初期化
@@ -27,9 +28,9 @@ window.cont = new (function(){
 					} );
 				}, 500);
 			});
-			_this.updatePageList(function(){
+			_this.redrawPageList( function(){
 				callback();
-			});
+			} );
 		});
 
 	}
@@ -91,7 +92,7 @@ window.cont = new (function(){
 	 */
 	function drawPageListTree(callback){
 
-		// console.log('/apis/getNavigationInfo?page_path='+encodeURIComponent(current_page));
+		console.log('/apis/getNavigationInfo?page_path='+encodeURIComponent(current_page));
 
 		$.get(
 			'/apis/getNavigationInfo?page_path='+encodeURIComponent(current_page),
@@ -161,180 +162,201 @@ window.cont = new (function(){
 	 * キーワードが指定されていたら、検索結果を表示する。
 	 */
 	function drawPageListSearch(callback){
-		var sitemap = _this.sitemap;
-		// console.log(sitemap);
-		var $ul = $('<table class="table table-striped table-sm table-hover cont_pagelist">');
-		$cont.html('').append( $('<div>')
-			.addClass('table-responsive')
-			.append($ul)
-		);
-		$ul
-			.append( $('<thead>')
-				.append( $('<tr>')
-					.append( $('<th>').text('ページID') )
-					.append( $('<th>').text('タイトル') )
-					.append( $('<th>').text('ページのパス') )
-					.append( $('<th>').text('担当者') )
-					.append( $('<th>').text('編集モード') )
-					.append( $('<th>').text('-') )
-					.append( $('<th>').text('-') )
-					.append( $('<th>').text('プレビュー') )
-				)
-			)
-		;
-
-		function isMatchKeywords(target){
-			if( typeof(target) != typeof('') ){
-				return false;
-			}
-			if( target.match(keyword) ){
-				return true;
-			}
-			return false;
-		}
-
-		for( var path in sitemap ){
-			(function($ul, sitemap, path){
-				if( keyword.length ){
-					if(
-						!isMatchKeywords(sitemap[path].id) &&
-						!isMatchKeywords(sitemap[path].path) &&
-						!isMatchKeywords(sitemap[path].content) &&
-						!isMatchKeywords(sitemap[path].title) &&
-						!isMatchKeywords(sitemap[path].title_breadcrumb) &&
-						!isMatchKeywords(sitemap[path].title_h1) &&
-						!isMatchKeywords(sitemap[path].title_label) &&
-						!isMatchKeywords(sitemap[path].title_full) &&
-						!isMatchKeywords(sitemap[path].assignee)
-					){
-						console.log('=> skiped.');
-						return;
-					}
+		it79.fnc({},[
+			function(it1, arg){
+				if( _this.sitemap !== false ){
+					// すでにサイトマップがロードされていたらスキップ
+					it1.next(arg);
+					return;
 				}
-
-
-				var $spanAssignee = $('<span>');
-				var $spanEditorType = $('<span>');
-				var $li = $('<tr>');
-				$li
-					.attr({
-						'data-page-path': path
-					})
-					.css({
-						'cursor': 'default'
-					})
-					.on('dblclick', function(e){
-						openEditor( $(this).attr('data-page-path') );
-						return false;
-					})
-					.append( $('<th>')
-						// ページID
-						.append( $('<span>')
-							.text(sitemap[path].id)
-						)
-					)
-					.append( $('<td>')
-						// タイトル
-						.append( $('<a>')
-							.text(sitemap[path].title)
-							.attr({
-								'href': 'javascript:;',
-								'data-page-path': path
-							})
-							.on('click', function(){
-								openEditor( $(this).attr('data-page-path') );
-								return false;
-							})
-						)
-					)
-					.append( $('<td>')
-						// パス
-						.append( $('<span>')
-							.text(sitemap[path].path)
-						)
-					)
-					.append( $('<td>')
-						// 担当者
-						.append( $spanAssignee.text((function(pageInfo){
-							// console.log(pageInfo);
-							var rtn = (pageInfo.assignee ? pageInfo.assignee : '---');
-							try {
-								rtn = (pageInfo.user_info.name + ' (' +  pageInfo.assignee + ')' || '---')
-							} catch (e) {
-							}
-							return rtn;
-						})( sitemap[path] )) )
-					)
-					.append( $('<td>')
-						// 編集モード
-						.append( $spanEditorType.html((function(editorType){
-							var editorTypeId = {
-								'html' : 'html',
-								'md' : 'md',
-								'txt' : 'txt',
-								'jade' : 'jade',
-								'html.gui' : 'html-gui',
-								'.not_exists' : 'not-exists',
-								'.page_not_exists' : 'page-not-exists'
-							};
-							var src = '<span class="px2-editor-type__'+editorTypeId[editorType]+' px2-editor-type--fullwidth"></span>';
-							return (editorTypeId[editorType] ? src : '---');
-						})( sitemap[path].editorType )) )
-					)
-					.append( $('<td>')
-						// コミットボタン
-						.append( $('<a>')
-							.attr({'href':'javascript:;'})
-							.on('click', function(){
-								px2dtGitUi.commit(
-									'contents',
-									{'page_path': path},
-									function(){
-										// alert('complete');
-									}
-								);
-							})
-							.text('コミット')
-						)
-					)
-					.append( $('<td>')
-						// ログボタン
-						.append( $('<a>')
-							.attr({'href':'javascript:;'})
-							.on('click', function(){
-								px2dtGitUi.log(
-									'contents',
-									{'page_path': path},
-									function(){
-										// alert('complete');
-									}
-								);
-							})
-							.text('ログ')
-						)
-					)
-					.append( $('<td>')
-						// プレビューボタン
-						.append( $('<a>')
-							.attr({'href':'javascript:;'})
-							.on('click', function(){
-								window.open( window.config.urlPreview+path );
-								return false;
-							})
-							.append( $('<span class="icn-preview">')
-								.text('見る')
-							)
+				_this.updatePageList(function(){
+					it1.next(arg);
+				});
+				return;
+			},
+			function(it1, arg){
+				var sitemap = _this.sitemap;
+				// console.log(sitemap);
+				var $ul = $('<table class="table table-striped table-sm table-hover cont_pagelist">');
+				$cont.html('').append( $('<div>')
+					.addClass('table-responsive')
+					.append($ul)
+				);
+				$ul
+					.append( $('<thead>')
+						.append( $('<tr>')
+							.append( $('<th>').text('ページID') )
+							.append( $('<th>').text('タイトル') )
+							.append( $('<th>').text('ページのパス') )
+							.append( $('<th>').text('担当者') )
+							.append( $('<th>').text('編集モード') )
+							.append( $('<th>').text('-') )
+							.append( $('<th>').text('-') )
+							.append( $('<th>').text('プレビュー') )
 						)
 					)
 				;
 
-				$ul.append($li);
+				function isMatchKeywords(target){
+					if( typeof(target) != typeof('') ){
+						return false;
+					}
+					if( target.match(keyword) ){
+						return true;
+					}
+					return false;
+				}
 
-			})($ul, sitemap, path);
+				for( var path in sitemap ){
+					(function($ul, sitemap, path){
+						if( keyword.length ){
+							if(
+								!isMatchKeywords(sitemap[path].id) &&
+								!isMatchKeywords(sitemap[path].path) &&
+								!isMatchKeywords(sitemap[path].content) &&
+								!isMatchKeywords(sitemap[path].title) &&
+								!isMatchKeywords(sitemap[path].title_breadcrumb) &&
+								!isMatchKeywords(sitemap[path].title_h1) &&
+								!isMatchKeywords(sitemap[path].title_label) &&
+								!isMatchKeywords(sitemap[path].title_full) &&
+								!isMatchKeywords(sitemap[path].assignee)
+							){
+								console.log('=> skiped.');
+								return;
+							}
+						}
 
-		}
-		callback();
-		return;
+
+						var $spanAssignee = $('<span>');
+						var $spanEditorType = $('<span>');
+						var $li = $('<tr>');
+						$li
+							.attr({
+								'data-page-path': path
+							})
+							.css({
+								'cursor': 'default'
+							})
+							.on('dblclick', function(e){
+								openEditor( $(this).attr('data-page-path') );
+								return false;
+							})
+							.append( $('<th>')
+								// ページID
+								.append( $('<span>')
+									.text(sitemap[path].id)
+								)
+							)
+							.append( $('<td>')
+								// タイトル
+								.append( $('<a>')
+									.text(sitemap[path].title)
+									.attr({
+										'href': 'javascript:;',
+										'data-page-path': path
+									})
+									.on('click', function(){
+										openEditor( $(this).attr('data-page-path') );
+										return false;
+									})
+								)
+							)
+							.append( $('<td>')
+								// パス
+								.append( $('<span>')
+									.text(sitemap[path].path)
+								)
+							)
+							.append( $('<td>')
+								// 担当者
+								.append( $spanAssignee.text((function(pageInfo){
+									// console.log(pageInfo);
+									var rtn = (pageInfo.assignee ? pageInfo.assignee : '---');
+									try {
+										rtn = (pageInfo.user_info.name + ' (' +  pageInfo.assignee + ')' || '---')
+									} catch (e) {
+									}
+									return rtn;
+								})( sitemap[path] )) )
+							)
+							.append( $('<td>')
+								// 編集モード
+								.append( $spanEditorType.html((function(editorType){
+									var editorTypeId = {
+										'html' : 'html',
+										'md' : 'md',
+										'txt' : 'txt',
+										'jade' : 'jade',
+										'html.gui' : 'html-gui',
+										'.not_exists' : 'not-exists',
+										'.page_not_exists' : 'page-not-exists'
+									};
+									var src = '<span class="px2-editor-type__'+editorTypeId[editorType]+' px2-editor-type--fullwidth"></span>';
+									return (editorTypeId[editorType] ? src : '---');
+								})( sitemap[path].editorType )) )
+							)
+							.append( $('<td>')
+								// コミットボタン
+								.append( $('<a>')
+									.attr({'href':'javascript:;'})
+									.on('click', function(){
+										px2dtGitUi.commit(
+											'contents',
+											{'page_path': path},
+											function(){
+												// alert('complete');
+											}
+										);
+									})
+									.text('コミット')
+								)
+							)
+							.append( $('<td>')
+								// ログボタン
+								.append( $('<a>')
+									.attr({'href':'javascript:;'})
+									.on('click', function(){
+										px2dtGitUi.log(
+											'contents',
+											{'page_path': path},
+											function(){
+												// alert('complete');
+											}
+										);
+									})
+									.text('ログ')
+								)
+							)
+							.append( $('<td>')
+								// プレビューボタン
+								.append( $('<a>')
+									.attr({'href':'javascript:;'})
+									.on('click', function(){
+										window.open( window.config.urlPreview+path );
+										return false;
+									})
+									.append( $('<span class="icn-preview">')
+										.text('見る')
+									)
+								)
+							)
+						;
+
+						$ul.append($li);
+
+					})($ul, sitemap, path);
+
+				}
+				it1.next(arg);
+				return;
+			},
+			function(it1, arg){
+				callback();
+			}
+		]);
+
+
+
 	}
 
 })();
