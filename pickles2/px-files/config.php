4b599a4baa6e5694a05c8e1aa83af27ce8fdb8ae
Tomoya Koyanagi <tomk79@gmail.com>
2017-06-04 17:27:55 +0900

    (ヘッダー要素の整理, ハンバーガーメニュー追加)

--- pickles2/px-files/config.php
+++ pickles2/px-files/config.php
@@ -5,7 +5,7 @@ return call_user_func( function(){
 	$conf = new stdClass;
 
 	// project
-	$conf->name = 'Pickles 2 Web Tool'; // サイト名
+	$conf->name = 'Pickles 2 Web Tool DEVSAMPLE'; // サイト名
 	$conf->domain = null; // ドメイン
 	$conf->path_controot = '/'; // コンテンツルートディレクトリ
 
--- src/common/scripts/main.js
+++ src/common/scripts/main.js
@@ -3,6 +3,7 @@ window.ejs = require('ejs');
 
 window.main = new (function(){
 	var _this = this;
+	var _it79 = require('iterate79');
 
 	this.progress = new (require('../../common/scripts/main.progress.js')).init(this, $);
 	this.message = require('../../common/scripts/main.message.js');
@@ -11,6 +12,8 @@ window.main = new (function(){
 	this.git = function(){
 		return new (require('../../common/scripts/main.project.git.js'))(this);
 	}
+
+	var $header, $contents, $footer, $shoulderMenu;
 
 	/**
 	 * ログアウトする
@@ -50,10 +53,60 @@ window.main = new (function(){
 	}
 
 	$(function(){
-		_this.project.getConfig(function(conf){
-			// console.log(conf);
-			$('header.theme-header .theme-header__ci__project-name').text(conf.name);
-		});
+		_it79.fnc({}, [
+			function(it, arg){
+				_this.project.getConfig(function(conf){
+					// console.log(conf);
+
+					$('header.theme-header .theme-header__id a').text(conf.name);
+					it.next(arg);
+				});
+			} ,
+			function(it, arg){
+
+				// DOMスキャン
+				$header   = $('.theme-header');
+				$contents = $('.contents');
+				$footer   = $('.theme-footer');
+				$shoulderMenu = $('.theme-header__shoulder-menu');
+
+				it.next(arg);
+
+			} ,
+			function(it, arg){
+				var $ul = $shoulderMenu.find('ul').hide();
+				$shoulderMenu
+					.css({
+						'width': 50,
+						'height': $header.height()
+					})
+					.on('click', function(){
+						if( $ul.css('display') == 'block' ){
+							$ul.hide();
+							$shoulderMenu
+								.css({
+									'width':50 ,
+									'height':$header.height()
+								})
+							;
+
+						}else{
+							$ul.show().height( $(window).height()-$header.height() );
+							$shoulderMenu
+								.css({
+									'width':'100%' ,
+									'height':$(window).height()
+								})
+							;
+
+						}
+					}
+				);
+				it.next(arg);
+			}
+		]);
+
+		window.focus();
 	});
 
 })();
