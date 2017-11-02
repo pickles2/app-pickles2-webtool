26c9c05e69d312b6fe46ed97963770da408d1128
hk-r <hiyoshi-kyota@imjp.co.jp>
2017-11-02 15:48:02 +0900

    コンテンツ編集完了ボタン押下後にプログレスバーを表示するよう修正

--- src/mods/editor/index_files/scripts/contents.js
+++ src/mods/editor/index_files/scripts/contents.js
@@ -87,6 +87,9 @@ $(window).load(function(){
 									return;
 								},
 								'complete': function(){
+									// progressの表示
+									window.main.progress.start({'blindness': true, 'showProgressBar': true});
+
 									$.ajax({
 										"url": "/apis/applock",
 										"type": 'post',
@@ -95,6 +98,9 @@ $(window).load(function(){
 											"page_path": params.page_path
 										},
 										"success": function(lockResult){
+											// プログレスの表示終了
+											window.main.progress.close();
+											
 											// console.log(lockResult);
 											if( !lockResult.result ){
 												alert('[ERROR] 編集状態の解除に失敗しました。');
